ea1b54f640e557099fdce76ae0e959276716210f
MOS2025 <michealose@gmail.com>
2026-06-09 09:04:35 +0100

    Inside the list view

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,10 @@ from .models import Post
 
 # Create your views here.
 
+#class PostList(generic.ListView):
+   # model = Post
+   # queryset = Post.objects.filter(author=3)
+
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(status=0)
+    template_name = "post_list.html"
